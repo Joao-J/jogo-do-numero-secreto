1f66b990b2363afbf0f32b9b224656a3b31ff867
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2024-01-28 11:22:35 -0300

    Update app.js
    atualização no botão de chute

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ function verificarChute(){
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('container__botao').setAttribute('disabled', true); /*Retirar o botão de chute após acertar*/
         document.getElementById('reiniciar'). removeAttribute('disabled');
     } else{
         if(chute > numeroSecreto){
@@ -62,5 +63,6 @@ function reiniciarJogo(){
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
+    document.getElementById('container__botao').removeAttribute('disabled'); /*Retornar o botão de chute após começar*/
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
